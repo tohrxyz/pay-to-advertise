7079e36a4d012189432df6ddb6033164d2ddd33c
tohrxyz <tomas.hrib@proton.me>
2023-11-25 17:45:25 +0100

    feat: add new endpoint which is called from btcpay as notification callback

--- index.ts
+++ index.ts
@@ -21,6 +21,31 @@ app.post('/open-url', (req, res) => {
     res.send('URL opened');
 });
 
+app.post('/paid', (req, res) => {
+  const status = req.body.status;
+  const posData = req.body.posData;
+  console.log("posData: ", posData);
+  
+  if (status == 'complete') {
+      console.log(req.body)
+      const url = posData.p2ad_url;
+      exec(`firefox ${url}`, (error, stdout, stderr) => {
+          if (error) {
+              console.log(`error: ${error.message}`);
+              return;
+          }
+          if (stderr) {
+              console.log(`stderr: ${stderr}`);
+              return;
+          }
+          console.log(`stdout: ${stdout}`);
+      });
+      res.send('paid');
+    } else {
+      res.send('not paid yet');
+    }
+});
+
 app.listen(3000, () => {
     console.log('Server listening on port 3000');
 });
